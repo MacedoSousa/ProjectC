50d9d39d4120d27e29f685fe2ed43575c9a61ad6
Pedro Macedo de Sousa <88333358+MacedoSousa@users.noreply.github.com>
2021-08-02 13:02:00 -0300

    First Project C#

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pasta Lógica
+namespace HelloWord { 
+
+//Função Primátia (Main)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Estou Voltando!");
+            
+        }
+    }
+}
